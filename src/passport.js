87d95878c6c04a2f5c6f5a8b38d7d9e311f74340
HAKJOON0223 <2hakjoon@gmail.com>
2020-08-09 17:14:40 +0900

    github

--- src/passport.js
+++ src/passport.js
@@ -10,7 +10,8 @@ passport.use(User.createStrategy());
 passport.use(new GithubStrategy({
     clientID : process.env.GH_ID,
     clientSecret : process.env.GH_SECRET,
-    callbackURL : `http://localhost:4000${routes.githubCallback}`,
+    callbackURL : process.env.PRODUCTION ? 
+    `https://damp-basin-93658.herokuapp.com/${routes.githubCallback}` : `http://localhost:4000${routes.githubCallback}`,
     },
     githubLoginCallback
     )
